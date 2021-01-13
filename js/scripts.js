f0099ab796b97fbb90665b60a09ddf033ce163ea
Maxwell Meyer <maxreadswell@gmail.com>
2021-01-12 20:25:38 -0800

    Update JS code to add click-to-reveal functionality for content section of all slides.

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,24 @@ $(document).ready(function() {
 
   reveal(".c1", ".t1")
   reveal(".c2", ".t2")
+  reveal(".c3", ".t3")
+  reveal(".c4", ".t4")
+  reveal(".c5", ".t5")
+  reveal(".c6", ".t6")
+  reveal(".c7", ".t7")
+  reveal(".c8", ".t8")
+  reveal(".c9", ".t9")
+  reveal(".c10", ".t10")
+  reveal(".c11", ".t11")
+  reveal(".c12", ".t12")
+  reveal(".c13", ".t13")
+  reveal(".c14", ".t14")
+  reveal(".c15", ".t15")
+  reveal(".c16", ".t16")
+  reveal(".c17", ".t17")
+  reveal(".c18", ".t18")
+  reveal(".c19", ".t19")
+
+  
 });
 
